9f00197185e0338d0dde2bc13b026459e37fa520
cprec-nepal <mthapaliya0512@gmail.com>
2026-01-12 12:38:38 +0545

    Update wsgi.py

--- bloodconnect_project/wsgi.py
+++ bloodconnect_project/wsgi.py
@@ -1,5 +1,13 @@
+"""
+WSGI config for bloodconnect_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+# pylint: disable=import-error
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bloodconnect_project.settings')
+
 application = get_wsgi_application()
